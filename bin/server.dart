1bc4f53a6ca5f65d731488d812c14c3d7c8f944f
Lentscode <antonio.lentini2004@gmail.com>
2024-08-21 14:09:41 +0200

    🚀 Defined server start script

--- bin/server.dart
+++ bin/server.dart
@@ -1,33 +1,30 @@
-import 'dart:io';
+import "dart:io";
 
-import 'package:shelf/shelf.dart';
-import 'package:shelf/shelf_io.dart';
-import 'package:shelf_router/shelf_router.dart';
+import "package:server/clothing.dart";
+import "package:shelf/shelf.dart";
+import "package:shelf/shelf_io.dart";
+import "package:shelf_router/shelf_router.dart";
 
 // Configure routes.
-final _router = Router()
-  ..get('/', _rootHandler)
-  ..get('/echo/<message>', _echoHandler);
+final Router _publicRouter = Router()
+  ..post("/login", login)
+  ..post("/register", register);
 
-Response _rootHandler(Request req) {
-  return Response.ok('Hello, World!\n');
-}
+final Router _privateRouter = Router();
 
-Response _echoHandler(Request request) {
-  final message = request.params['message'];
-  return Response.ok('$message\n');
-}
+final Router _router = Router()
+  ..mount("/public", _publicRouter.call)
+  ..mount("/protected", _privateRouter.call);
 
 void main(List<String> args) async {
   // Use any available host or container IP (usually `0.0.0.0`).
-  final ip = InternetAddress.anyIPv4;
+  final InternetAddress ip = InternetAddress.anyIPv4;
 
   // Configure a pipeline that logs requests.
-  final handler =
-      Pipeline().addMiddleware(logRequests()).addHandler(_router.call);
+  final Handler handler = const Pipeline().addMiddleware(logRequests()).addHandler(_router.call);
 
   // For running in containers, we respect the PORT environment variable.
-  final port = int.parse(Platform.environment['PORT'] ?? '8080');
-  final server = await serve(handler, ip, port);
-  print('Server listening on port ${server.port}');
+  final int port = int.parse(Platform.environment["PORT"] ?? "8080");
+  final HttpServer server = await serve(handler, ip, port);
+  print("Server listening on port ${server.port}");
 }
