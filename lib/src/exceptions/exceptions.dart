450e19372f5303ee399ae90677389cf24395eae7
Lentscode <antonio.lentini2004@gmail.com>
2024-08-21 11:43:52 +0200

    🥅 Added exceptions

--- lib/src/exceptions/exceptions.dart
+++ lib/src/exceptions/exceptions.dart
@@ -1,1 +1,19 @@
 library;
+
+class InvalidCredentialsException implements Exception {
+  @override
+  String toString() => "Invalid credentials";
+}
+
+class UserWithEmailAlreadyExistingException implements Exception {
+  @override
+  String toString() => "User with email already exists";
+}
+
+class ObjectNotFoundException implements Exception {
+  const ObjectNotFoundException(this.type);
+
+  final String type;
+  @override
+  String toString() => "$type not found";
+}
