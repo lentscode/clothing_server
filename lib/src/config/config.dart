58f2a9e740708518b1092f003680b004f7163c16
Lentscode <antonio.lentini2004@gmail.com>
2024-08-21 14:09:06 +0200

    🔨 Added pre-run config script
    This function will be called before the server starts

--- lib/src/config/config.dart
+++ lib/src/config/config.dart
@@ -1,1 +1,22 @@
 library;
+
+import "package:get_it/get_it.dart";
+import "package:mongo_dart/mongo_dart.dart";
+
+import "../../clothing.dart";
+import "../logic/auth/auth.dart";
+
+final GetIt getIt = GetIt.instance;
+
+Future<void> config() async {
+  final Db db = await Db.create("");
+  await db.open();
+
+  final Auth auth = Auth(db.collection("users"));
+  final ClothingDataSource clothingDataSource = ClothingDataSource(db.collection("clothings"));
+  final OutfitDataSource outfitDataSource = OutfitDataSource(db.collection("outfits"));
+
+  getIt.registerSingleton<Auth>(auth);
+  getIt.registerSingleton<ClothingDataSource>(clothingDataSource);
+  getIt.registerSingleton<OutfitDataSource>(outfitDataSource);
+}
