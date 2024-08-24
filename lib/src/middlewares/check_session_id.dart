227250faec666041f65284f74600c9138f2ee8e5
Lentscode <antonio.lentini2004@gmail.com>
2024-08-24 08:51:19 +0200

    🎨 Changed name and formatted code

--- lib/src/middlewares/check_session_id.dart
+++ lib/src/middlewares/check_session_id.dart
@@ -1,13 +1,14 @@
 part of "middlewares.dart";
 
 /// A middleware that checks if a session ID inside the cookies is valid.
-Middleware checkCookie() => (Handler innerHandler) => (Request req) async {
+Middleware checkSessionId() => (Handler innerHandler) => (Request req) async {
       try {
         final String sessionId = RequestUtils(req).getSessionId();
 
         final User user = await getIt.get<Auth>().checkSessionId(sessionId);
 
-        final Request newReq = req.change(context: <String, Object>{"user": user});
+        final Request newReq =
+            req.change(context: <String, Object>{"user": user});
 
         return innerHandler(newReq);
       } on SessionIdNotValidException catch (e) {
