e3d7d027acf3282940b5473f396d62c64493e2c2
Lentscode <antonio.lentini2004@gmail.com>
2024-08-25 19:26:17 +0200

    🔧 Updated Google config map

--- lib/src/config/credentials/credentials.impl.dart
+++ lib/src/config/credentials/credentials.impl.dart
@@ -12,17 +12,19 @@ class _CredentialsImpl implements Credentials {
 
   @override
   // TODO: implement googleServiceAccount
-  Map<String, String> get googleServiceAccount => {
-    "type": "service_account",
-    "project_id": _env["GOOGLE_PROJECT_ID"] ?? "",
-    "private_key_id": _env["GOOGLE_PRIVATE_KEY_ID"] ?? "",
-    "private_key": _env["GOOGLE_PRIVATE_KEY"] ?? "",
-    "client_email": _env["GOOGLE_CLIENT_EMAIL"] ?? "",
-    "client_id": _env["GOOGLE_CLIENT_ID"] ?? "",
-    "auth_uri": _env["GOOGLE_AUTH_URI"] ?? "",
-    "token_uri": _env["GOOGLE_TOKEN_URI"] ?? "",
-    "auth_provider_x509_cert_url": _env["GOOGLE_AUTH_PROVIDER_X509_CERT_URL"] ?? "",
-    "client_x509_cert_url": _env["GOOGLE_CLIENT_X509_CERT_URL"] ?? "",
-    "universe_domain": _env["GOOGLE_UNIVERSE_DOMAIN"] ?? "",
-  };
+  Map<String, String> get googleServiceAccount => <String, String>{
+        "type": "service_account",
+        "project_id": _env["GOOGLE_PROJECT_ID"] ?? "",
+        "private_key_id": _env["GOOGLE_PRIVATE_KEY_ID"] ?? "",
+        "private_key":
+            (_env["GOOGLE_PRIVATE_KEY"] ?? "").replaceAll(r"\n", "\n"),
+        "client_email": _env["GOOGLE_CLIENT_EMAIL"] ?? "",
+        "client_id": _env["GOOGLE_CLIENT_ID"] ?? "",
+        "auth_uri": _env["GOOGLE_AUTH_URI"] ?? "",
+        "token_uri": _env["GOOGLE_TOKEN_URI"] ?? "",
+        "auth_provider_x509_cert_url":
+            _env["GOOGLE_AUTH_PROVIDER_X509_CERT_URL"] ?? "",
+        "client_x509_cert_url": _env["GOOGLE_CLIENT_X509_CERT_URL"] ?? "",
+        "universe_domain": _env["GOOGLE_UNIVERSE_DOMAIN"] ?? "",
+      };
 }
