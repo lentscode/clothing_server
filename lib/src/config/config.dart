2d6ce9a803bb87fd78590f8a423d401c12d4f61f
Lentscode <antonio.lentini2004@gmail.com>
2024-08-25 12:03:19 +0200

    🔨 Updated config()
    Added script to add 'CloudStorage' to getIt.

--- lib/src/config/config.dart
+++ lib/src/config/config.dart
@@ -1,6 +1,8 @@
 library;
 
 import "package:get_it/get_it.dart";
+import "package:googleapis/storage/v1.dart";
+import "package:googleapis_auth/auth_io.dart";
 import "package:mongo_dart/mongo_dart.dart";
 
 import "../../server.dart";
@@ -12,8 +14,17 @@ final GetIt getIt = GetIt.instance;
 
 /// Executes code before server start.
 Future<void> config([bool testing = false]) async {
-  final Db db = await Db.create(testing ? Credentials().mongoUriTest : Credentials().mongoUri);
+  final Db db = await Db.create(
+      testing ? Credentials().mongoUriTest : Credentials().mongoUri);
   await db.open();
+
+  final ServiceAccountCredentials googleCredentials =
+      ServiceAccountCredentials.fromJson(Credentials().googleServiceAccount);
+
+  final AutoRefreshingAuthClient googleClient = await clientViaServiceAccount(
+      googleCredentials, <String>[StorageApi.devstorageReadWriteScope]);
+
+  final StorageApi storageApi = StorageApi(googleClient);
 
   final Auth auth = Auth(db.collection("users"));
   final ClothingDataSource clothingDataSource =
@@ -21,7 +32,10 @@ Future<void> config([bool testing = false]) async {
   final OutfitDataSource outfitDataSource =
       OutfitDataSource(db.collection("outfits"));
 
+  final CloudStorage cloudStorage = CloudStorage(storageApi);
+
   getIt.registerSingleton<Auth>(auth);
   getIt.registerSingleton<ClothingDataSource>(clothingDataSource);
   getIt.registerSingleton<OutfitDataSource>(outfitDataSource);
+  getIt.registerSingleton<CloudStorage>(cloudStorage);
 }
