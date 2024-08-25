f25bf8323406cd0a798c7d98864013e8466a9a91
Lentscode <antonio.lentini2004@gmail.com>
2024-08-25 19:28:03 +0200

    👔 Updated outfit retrieval
    Now every Outfit object comes with its Clothing objects

--- lib/src/data/outfit_data_source/outfit_data_source.dart
+++ lib/src/data/outfit_data_source/outfit_data_source.dart
@@ -7,19 +7,21 @@ part "outfit_data_source.impl.dart";
 
 /// A class to work with outfit data in the database.
 abstract class OutfitDataSource {
-  const OutfitDataSource._(this.outfits);
+  const OutfitDataSource._(this.outfits, this.clothingDataSource);
 
   /// Returns a new instance of [OutfitDataSource].
-  factory OutfitDataSource(DbCollection outfits) = _OutfitDataSourceImpl;
+  factory OutfitDataSource(DbCollection outfits, ClothingDataSource clothingDataSource) = _OutfitDataSourceImpl;
 
   /// The [DbCollection] of outfits in MongoDB.
   final DbCollection outfits;
+
+  final ClothingDataSource clothingDataSource;
 
   /// Creates a new [Outfit] in the database.
   Future<Outfit> createOutfit(Outfit outfit);
 
   /// Gets an [Outfit] with a given [id].
-  /// 
+  ///
   /// Throws an [ObjectNotFoundException] if no outfit with the given [id] was found.
   Future<Outfit> getOutfit(ObjectId id);
 
--- lib/src/data/outfit_data_source/outfit_data_source.impl.dart
+++ lib/src/data/outfit_data_source/outfit_data_source.impl.dart
@@ -1,11 +1,12 @@
 part of "outfit_data_source.dart";
 
 class _OutfitDataSourceImpl extends OutfitDataSource {
-  const _OutfitDataSourceImpl(super.outfits) : super._();
+  const _OutfitDataSourceImpl(super.outfits, super.clothingDataSource)
+      : super._();
 
   @override
   Future<Outfit> createOutfit(Outfit outfit) =>
-      outfits.insert(outfit.toMap()).then((_) => outfit);
+      outfits.insert(outfit.toMongo()).then((_) => outfit);
 
   @override
   Future<Outfit> getOutfit(ObjectId id) async {
@@ -15,14 +16,39 @@ class _OutfitDataSourceImpl extends OutfitDataSource {
       throw const ObjectNotFoundException("Outfit");
     }
 
+    final List<dynamic> clothingsIds = map["clothings"];
+
+    final List<Clothing> clothings = await _getClothingsOfOutfit(clothingsIds);
+
+    map["clothings"] = clothings;
+
     return Outfit.fromMap(map);
   }
 
   @override
-  Future<List<Outfit>> getOutfitsOfUser(ObjectId userId) {
+  Future<List<Outfit>> getOutfitsOfUser(ObjectId userId) async {
     final Stream<Map<String, dynamic>> maps =
-        outfits.find(where.eq("userId", userId));
+        outfits.find(where.eq("userId", userId.oid));
 
-    return maps.map(Outfit.fromMap).toList();
+    List<Map<String, dynamic>> mapsList = await maps.toList();
+
+    for (final Map<String, dynamic> map in mapsList) {
+      final List<dynamic> clothingsIds = map["clothings"];
+
+      final List<Clothing> clothings =
+          await _getClothingsOfOutfit(clothingsIds);
+
+      map["clothings"] = clothings;
+    }
+
+    return mapsList.map(Outfit.fromMap).toList();
   }
+
+  Future<List<Clothing>> _getClothingsOfOutfit(
+    List<dynamic> clothingsIds,
+  ) async =>
+      Future.wait(
+        clothingsIds.map(
+            (dynamic id) => clothingDataSource.getClothing(ObjectId.parse(id))),
+      );
 }
