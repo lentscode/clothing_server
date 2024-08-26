e58bfcfe36da461b5ada73126232188dde2837d8
Lentscode <antonio.lentini2004@gmail.com>
2024-08-26 15:33:26 +0200

    👔 Updated method
    Now, instead of returning the image directly via the response, we provide a signed URL to the user to download the image from the client.

--- lib/src/data/cloud_storage/cloud_storage.dart
+++ lib/src/data/cloud_storage/cloud_storage.dart
@@ -1,9 +1,21 @@
+// ignore_for_file: implementation_imports
+
+import "dart:collection";
+import "dart:convert";
 import "dart:io";
 import "dart:typed_data";
 
+import "package:crypto/crypto.dart";
 import "package:googleapis/storage/v1.dart";
+import "package:googleapis_auth/src/crypto/pem.dart";
+import "package:googleapis_auth/src/crypto/rsa.dart";
+import "package:googleapis_auth/src/crypto/rsa_sign.dart";
+import "package:hex/hex.dart";
+import "package:intl/intl.dart";
 import "package:shared/shared.dart";
 import "package:uuid/uuid.dart";
+
+import "../../../server.dart";
 
 part "cloud_storage.impl.dart";
 
@@ -14,5 +26,11 @@ abstract class CloudStorage {
 
   final StorageApi _api;
 
-  Future<String?> uploadImage(File image, User user, String imageType);
+  Future<String?> uploadImage(
+    File image,
+    User user,
+    String imageType, {
+    String bucketName = "clothing-test",
+    bool deleteFile = true,
+  });
 }
--- lib/src/data/cloud_storage/cloud_storage.impl.dart
+++ lib/src/data/cloud_storage/cloud_storage.impl.dart
@@ -4,7 +4,13 @@ class _CloudStorageImpl extends CloudStorage {
   _CloudStorageImpl(super._api) : super._();
 
   @override
-  Future<String?> uploadImage(File image, User user, String imageType) async {
+  Future<String?> uploadImage(
+    File image,
+    User user,
+    String imageType, {
+    String bucketName = "clothing-app",
+    bool deleteFile = true,
+  }) async {
     final String path = "$imageType/${user.oid}/${const Uuid().v4()}";
 
     final Uint8List imageBytes = await image.readAsBytes();
@@ -22,6 +28,111 @@ class _CloudStorageImpl extends CloudStorage {
       uploadMedia: media,
     );
 
-    return objectLink.selfLink;
+    final String signedUrl = _generateSignedUrl(
+      Credentials().googleServiceAccount,
+      "clothing-app",
+      objectLink.name!,
+    );
+
+    if (deleteFile) {
+      image.delete();
+    }
+
+    return signedUrl;
+  }
+
+  String _generateSignedUrl(
+    Map<String, String> serviceAccount,
+    String bucketName,
+    String objectName, {
+    String? subresource,
+    int expiration = 604800,
+    String httpMethod = "GET",
+    Map<String, String>? queryParameters,
+    Map<String, String>? headers,
+  }) {
+    if (expiration > 604800) {
+      print("Expiration Time can't be longer than 604800 seconds (7 days).");
+      return "";
+    }
+
+    // Encoding the object name
+    final String escapedObjectName = Uri.encodeComponent(objectName);
+    final String canonicalUri = "/$escapedObjectName";
+
+    final DateTime dateTimeNow = DateTime.now().toUtc();
+    final String requestTimestamp =
+        DateFormat("yyyyMMdd'T'HHmmss'Z'").format(dateTimeNow);
+    final String datestamp = DateFormat("yyyyMMdd").format(dateTimeNow);
+
+    // Load your Google credentials and extract the client email
+    final String? clientEmail =
+        serviceAccount["client_email"]; // Extract from service account file
+    final String credentialScope = "$datestamp/auto/storage/goog4_request";
+    final String credential = "$clientEmail/$credentialScope";
+
+    headers ??= <String, String>{};
+    final String host = "$bucketName.storage.googleapis.com";
+    headers["host"] = host;
+
+    SplayTreeMap splayTreeMap = SplayTreeMap.from(
+        headers); //this orders the headers alphabetically, important!
+    headers = Map.from(splayTreeMap);
+
+    final String canonicalHeaders = headers.entries
+        .map((MapEntry<String, String> entry) =>
+            "${entry.key.toLowerCase()}:${entry.value.trim().toLowerCase()}\n")
+        .join();
+    final String signedHeaders =
+        headers.keys.map((String k) => k.toLowerCase()).join(";");
+
+    queryParameters ??= <String, String>{};
+    queryParameters.addAll(<String, String>{
+      "X-Goog-Algorithm": "GOOG4-RSA-SHA256",
+      "X-Goog-Credential": credential,
+      "X-Goog-Date": requestTimestamp,
+      "X-Goog-Expires": expiration.toString(),
+      "X-Goog-SignedHeaders": signedHeaders,
+    });
+    if (subresource != null) {
+      queryParameters[subresource] = "";
+    }
+
+    final String canonicalQueryString = queryParameters.entries
+        .map((MapEntry<String, String> entry) =>
+            "${Uri.encodeComponent(entry.key)}=${Uri.encodeComponent(entry.value)}")
+        .join("&");
+
+    final String canonicalRequest = <String>[
+      httpMethod,
+      canonicalUri,
+      canonicalQueryString,
+      canonicalHeaders,
+      signedHeaders,
+      "UNSIGNED-PAYLOAD",
+    ].join("\n");
+
+    final String canonicalRequestHash =
+        sha256.convert(utf8.encode(canonicalRequest)).toString();
+
+    final String stringToSign = <String>[
+      "GOOG4-RSA-SHA256",
+      requestTimestamp,
+      credentialScope,
+      canonicalRequestHash,
+    ].join("\n");
+
+    String? pem = serviceAccount["private_key"];
+    RSAPrivateKey rsaKey = keyFromString(pem!);
+    RS256Signer signer = RS256Signer(rsaKey);
+
+    List<int> stringToSignList = utf8.encode(stringToSign);
+    List<int> signedRequestBytes = signer.sign(stringToSignList);
+    String signature = HEX.encode(signedRequestBytes);
+
+    final String schemeAndHost = "https://$host";
+    final String signedUrl =
+        "$schemeAndHost$canonicalUri?$canonicalQueryString&x-goog-signature=$signature";
+    return signedUrl;
   }
 }
